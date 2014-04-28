2155274ae38e688999f5a6dee3b49505ceb49da5
MherBaghinyan <mbaghinyan@gmail.com>
2014-04-28 19:52:40 +0500

    Update regExp.js

--- regExp.js
+++ regExp.js
@@ -1,1 +1,15 @@
 //regExp.js
+
+// validate hex color
+var validateHexColor = function(hexColor)
+{
+     var validator = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;   
+     return validator.test(hexColor);
+};
+
+// validate GUID
+var validateGUID = function(token)
+{
+     var validator = /^(\{|\()?[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}(\}|\))?$/;   
+     return validator.test(token);
+};
