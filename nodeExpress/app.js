ba7d94027b27ecb5243765acbb57ed7e00cf83ca
MherBaghinyan <mbaghinyan@gmail.com>
2014-05-28 21:42:13 +0500

    testing hello world

--- nodeExpress/app.js
+++ nodeExpress/app.js
@@ -33,6 +33,11 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
+app.get('/test', function(req, res, next)
+{
+    res.send("Hello world");
+});
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
